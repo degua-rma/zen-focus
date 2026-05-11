825f7f6e2ee0a0da45093485e3eb5ddc04f2af8a
s88431ss <ss88431ss@gmail.com>
2026-05-11 10:44:31 +0800

    feat: 整理SCSS結構與設定

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,20 +2,31 @@
   <div class="login-view linear-bg">
     <div class="login-info glass-card">
       <div class="logo">ZEN FOCUS</div>
-      <input type="text" placeholder="Email or Username" class="glass-input" />
-      <input type="password" placeholder="Enter Password" class="glass-input" />
+      <input
+        type="text"
+        name="email"
+        placeholder="Email or Username"
+        class="glass-input"
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="Enter Password"
+        class="glass-input"
+      />
       <div class="text-right">
-        <a href="" class="forgot-password"
-          ><el-icon><QuestionFilled /></el-icon>Forgot Password?</a
-        >
+        <a href="" class="forgot-password">
+          <el-icon class="mr-1"><QuestionFilled /></el-icon>
+          <span>Forgot Password?</span>
+        </a>
       </div>
-      <button class="glass-button">SIGN IN</button>
-      <div class="divider">OR</div>
-      <button class="other-btn">
+      <button class="glass-main-button">SIGN IN</button>
+      <div class="text-divider">OR</div>
+      <button class="glass-other-btn">
         <IconGoogle class="button-icon" />
         <span>Continue with Google</span>
       </button>
-      <button class="other-btn">
+      <button class="glass-other-btn">
         <el-icon class="button-icon"><Avatar /></el-icon>
         <span>Continue as Guest</span>
       </button>
@@ -37,6 +48,12 @@ import IconGoogle from "@/components/icons/IconGoogle.vue";
   opacity: 0.5;
 }
 
+.linear-bg {
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(135deg, $light-color, $main-color);
+}
+
 .login-view {
   width: 100vw;
   height: 100vh;
@@ -48,5 +65,19 @@ import IconGoogle from "@/components/icons/IconGoogle.vue";
 .login-info {
   width: 100%;
   max-width: 500px;
+}
+
+.forgot-password {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  font-size: $font-size-h5;
+  color: $text-color;
+  opacity: 0.6;
+  text-decoration: none;
+  transition: all 0.3s ease;
+  &:hover {
+    opacity: 1;
+  }
 }
 </style>
