c93d337d1d89a2ee216d6b3429b88fa3e6bb1237
degua-rma <ss88431ss@gmail.com>
2026-06-09 11:44:51 +0800

    feat: 調整佈局

--- src/layout/MainLayout.vue
+++ src/layout/MainLayout.vue
@@ -1,27 +1,32 @@
 <template>
   <div class="main-layout">
     <div class="main-header">
-      <a id="logo" href="/">ZEN FOCUS</a>
-      <div class="header-wrap">
-        <BreadCrumb />
-        <div class="flex items-center gap-2">
-          <LoginStatus />
-          <ChangeLang type="dashboard" />
+      <div class="card">
+        <a id="logo" href="/">ZEN FOCUS</a>
+        <div class="header-wrap">
+          <BreadCrumb />
+          <div class="flex items-center gap-2">
+            <LoginStatus />
+            <ChangeLang type="dashboard" />
+          </div>
         </div>
       </div>
     </div>
     <div class="main-container">
       <div class="main-nav">
-        <NavBar />
+        <div class="card">
+          <el-scrollbar>
+            <NavBar />
+          </el-scrollbar>
+        </div>
       </div>
       <div class="main-inner">
         <div class="main-page">
-          <el-scrollbar>
-            <router-view />
-          </el-scrollbar>
-        </div>
-        <div class="main-footer">
-          <p class="text-h6">版本資訊：v0 - 聯絡資訊</p>
+          <div class="card">
+            <el-scrollbar>
+              <router-view />
+            </el-scrollbar>
+          </div>
         </div>
       </div>
     </div>
@@ -38,28 +43,49 @@ import LoginStatus from "@/layout/LoginStatus.vue";
 <style lang="scss">
 .main-layout {
   width: 100%;
+  background: linear-gradient(
+    135deg,
+    var(--el-color-primary),
+    var(--el-color-primary-light-5)
+  );
+  padding: calc($inner-padding / 2);
+  .card {
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(135deg, rgba(#fff, 0.9), rgba(#fff, 0.7));
+    border: 1px solid var(--el-color-primary-light-9);
+    border-radius: calc($border-radius / 2);
+    box-shadow: none;
+  }
   .main-header {
     width: 100%;
-    height: $header-height;
-    padding: 0;
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
-    border-bottom: 1px solid #ddd;
+    padding: $inner-padding;
+    .card {
+      width: 100%;
+      height: calc($header-height - $inner-padding * 3);
+      padding: 0;
+      display: flex;
+      align-items: center;
+      justify-content: flex-start;
+      flex-direction: row;
+    }
     #logo {
-      width: $nav-width;
-      height: $header-height;
+      width: calc($nav-width - $inner-padding / 2);
+      height: 100%;
+      margin-right: calc($inner-padding / 2);
+      border-right: 1px dashed var(--el-color-primary-light-7);
+      padding: 0;
       display: inline-flex;
       align-items: center;
       justify-content: center;
       font-size: $font-size-h2;
       font-style: italic;
       letter-spacing: $md-spacing;
-      border-right: 1px solid #ddd;
     }
     .header-wrap {
       width: calc(100% - $nav-width);
-      padding: 0 $inner-padding;
+      height: 100%;
+      padding: 0 calc($inner-padding * 2);
       display: flex;
       align-items: center;
       justify-content: space-between;
@@ -73,25 +99,29 @@ import LoginStatus from "@/layout/LoginStatus.vue";
     .main-nav {
       width: $nav-width;
       height: calc(100vh - $header-height);
-      border-right: 1px solid #ddd;
+      padding: $inner-padding;
+      padding-top: 0;
+      padding-right: 0;
+      .card {
+        padding: $inner-padding 0;
+        overflow: hidden;
+      }
       .el-menu {
         border-right: none;
+        background-color: transparent;
       }
     }
     .main-inner {
       width: calc(100% - $nav-width);
+      height: calc(100vh - $header-height);
+      padding: $inner-padding;
+      padding-top: 0;
       .main-page {
         width: 100%;
-        height: calc(100vh - $header-height - $footer-height);
-        padding: $inner-padding;
-      }
-      .main-footer {
-        width: 100%;
-        height: $footer-height;
-        border-top: 1px solid #ddd;
-        display: flex;
-        align-items: center;
-        justify-content: center;
+        height: 100%;
+        .card {
+          padding: calc($inner-padding * 2);
+        }
       }
     }
   }
